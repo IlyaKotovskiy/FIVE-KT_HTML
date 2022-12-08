6476a905b97e80442a974c39c7002e139d9e3825
IlyaKotovskiy <KotovskiiIV21@st.ithub.ru>
2022-12-08 23:26:44 +0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    document.body.classList.add('noneMoon'),
+    document.body.classList.add('changeBackground'),
+    document.querySelector('.meteorites').classList.add('active')
+}, 15000);
+
+setTimeout(() => {
+    document.querySelector('.fin').classList.add('active')
+}, 24000);
